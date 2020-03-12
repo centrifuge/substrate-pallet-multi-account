38198e0e7a333e2b1f7f4389e628a4c2aba2fa12
Jovansonlee Cesar <ivanceras@gmail.com>
2020-03-12 20:07:24 +0800

    Add travis-ci config
    Note: the --locked flag in cargo build was remove, since it was causing a build error, the secret is also commented out

---
+++ ci/script.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -eux
+
+RUST_TOOLCHAIN="${RUST_TOOLCHAIN:-nightly}"
+
+# Enable warnings about unused extern crates
+export RUSTFLAGS=" -W unused-extern-crates"
+
+# Install rustup and the specified rust toolchain.
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+# Load cargo environment. Specifically, put cargo into PATH.
+source ~/.cargo/env
+
+sudo apt-get -y update
+sudo apt-get install -y cmake pkg-config libssl-dev
+
+./scripts/init.sh
+
+rustc --version
+rustup --version
+cargo --version
+
+case $TARGET in
+	"build-client")
+		cargo build --release  "$@"
+		;;
+
+	"runtime-test")
+		cargo test -p substrate-pallet-multi-account
+		wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
+        tar xzf master.tar.gz &&
+        cd kcov-master &&
+        mkdir build &&
+        cd build &&
+        cmake .. &&
+        make &&
+        make install DESTDIR=../../kcov-build &&
+        cd ../.. &&
+        rm -rf kcov-master &&
+        for file in target/debug/substrate_pallet_multi_account*; do [ -x "${file}" ] || continue; mkdir -p "target/cov/$(basename $file)"; kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done &&
+        bash <(curl -s https://codecov.io/bash) &&
+        echo "Uploaded code coverage"
+		;;
+esac
