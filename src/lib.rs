a99ee8f5c95818f012431029595b25642d02df08
vedhavyas <vedhavyas.singareddi@gmail.com>
2020-07-08 12:34:36 +0530

    fix fmt

--- src/lib.rs
+++ src/lib.rs
@@ -72,8 +72,8 @@
 
 use codec::{Decode, Encode};
 use frame_support::{
-    decl_error, decl_event, decl_module, decl_storage, ensure, Parameter, RuntimeDebug,
-    storage::IterableStorageDoubleMap
+    decl_error, decl_event, decl_module, decl_storage, ensure, storage::IterableStorageDoubleMap,
+    Parameter, RuntimeDebug,
 };
 use frame_support::{
     traits::{Currency, Get, ReservableCurrency},
